8f223d5e091e151d7903d6c1177d571da0374120
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-20 09:36:07 -0800

    Move to ~ instead of test directory

--- common.sh
+++ common.sh
@@ -9,6 +9,6 @@ command "$PKGMG install tmux vim git zsh"
 for f in *; do
     if [[ "./$f" != $0 && ! -d "$f" && "$f" != "manager" ]]; then
         echo "Moving $f to ~/.$f"
-        command ln $f "test/.$f"
+        command ln $f "~/.$f"
     fi
 done
