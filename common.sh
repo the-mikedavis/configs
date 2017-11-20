b700051b538c45fb28f39d6606036fa8a2c662c6
Michael Davis <rubiksmaster65@yahoo.com>
2017-11-20 09:35:43 -0800

    Add some instructions and restore the common code

--- common.sh
+++ common.sh
@@ -4,10 +4,10 @@ source manager
 
 echo "Using package manager: $PKGMG"
 
-#command "$PKGMG install tmux vim git zsh"
+command "$PKGMG install tmux vim git zsh"
 
 for f in *; do
-    if [[ "./$f" != $0 && ! -d "$f" ]]; then
+    if [[ "./$f" != $0 && ! -d "$f" && "$f" != "manager" ]]; then
         echo "Moving $f to ~/.$f"
         command ln $f "test/.$f"
     fi
---
+++ zsh/README.md
@@ -0,0 +1,5 @@
+Add this line to the .zshrc **after* installing oh-my-zsh:
+
+```sh
+source .aliases
+```
