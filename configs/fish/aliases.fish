610437b01bcbfa0cd0e155757dd9651b193b8344
Michael Davis <mcd164@miami.edu>
2018-06-16 14:25:03 -0400

    ChChCh-changes

--- configs/fish/aliases.fish
+++ configs/fish/aliases.fish
@@ -14,12 +14,24 @@ alias cr="clear"
 funcsave cr
 alias ty="type"
 funcsave ty
+alias aoeu="asdf"
+funcsave aoeu
 
 function ve --description 'read everything in tabs'
-  if set -q $argv[1]
+  if test -d $argv[1]
+    vim -p $argv[1]*
+  else if set -q $argv[1]
     vim -p *
   else
     vim -p $argv[1]
   end
 end
 funcsave ve
+
+function gerge --description 'take in a github merge locally'
+  set branch (git rev-parse --abbrev-ref HEAD)
+  git checkout master
+  git pull
+  git branch -d $branch
+end
+funcsave gerge
